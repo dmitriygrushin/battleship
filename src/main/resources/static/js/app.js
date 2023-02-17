59d8fe1a7a70c003dcd7ba4dfda5326e0a110c44
Dmitriy Grushin <dmitriy_grushin@yahoo.com>
2023-02-17 18:14:59 -0500

    Refactor: Client-Side JS

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -7,36 +7,12 @@ let isReady = false;
 let isGameFinished = false;
 let sound = false;
 
-
 // Board: |0 - ocean|, |1 - hit|, |2 - miss|,  |3 - ship|
 
+// 10x10 arrays
 // You don't get the actual opponents board. This is more of a reference board
-let opponentBoard = 
-//cols           A,B,C,D,E,F,G,H,I,J    
-			[	[0,0,0,0,0,0,0,0,0,0],  // 1 rows
-				[0,0,0,0,0,0,0,0,0,0],  // 2 
-				[0,0,0,0,0,0,0,0,0,0],  // 3 
-				[0,0,0,0,0,0,0,0,0,0],  // 4 
-				[0,0,0,0,0,0,0,0,0,0],  // 5 
-				[0,0,0,0,0,0,0,0,0,0],  // 6 
-				[0,0,0,0,0,0,0,0,0,0],  // 7 
-				[0,0,0,0,0,0,0,0,0,0],  // 8  
-				[0,0,0,0,0,0,0,0,0,0],  // 9  
-				[0,0,0,0,0,0,0,0,0,0]]; // 10 
-
-
-let myBoard = 
-//cols           A,B,C,D,E,F,G,H,I,J    row:
-			[	[0,0,0,0,0,0,0,0,0,0],  // 1 
-				[0,0,0,0,0,0,0,0,0,0],  // 2 
-				[0,0,0,0,0,0,0,0,0,0],  // 3 
-				[0,0,0,0,0,0,0,0,0,0],  // 4 
-				[0,0,0,0,0,0,0,0,0,0],  // 5 
-				[0,0,0,0,0,0,0,0,0,0],  // 6 
-				[0,0,0,0,0,0,0,0,0,0],  // 7 
-				[0,0,0,0,0,0,0,0,0,0],  // 8  
-				[0,0,0,0,0,0,0,0,0,0],  // 9  
-				[0,0,0,0,0,0,0,0,0,0]]; // 10 
+const opponentBoard = Array.from({ length: 10 }, () => Array(10).fill(0)); 
+const myBoard = Array.from({ length: 10 }, () => Array(10).fill(0)); 
 
 let roomId = (() => {
 	const queryString = window.location.search;
@@ -57,7 +33,7 @@ function setConnected(connected) {
         $("#conversation").show();
     } else {
         $("#conversation").hide();
-        removeOpponentUsername();
+		$("#p-vs-p").text("Waiting for opponent...");
     }
     $("#messages").html("");
 }
@@ -69,77 +45,74 @@ function connect() {
         setConnected(true);
         console.log('Connected: ' + frame);
         
-        drawPrepBoard();
+        drawBoard("prep-board", myBoard);
         
         stompClient.subscribe(`/user/topic/${roomId}`, async (message) => {
 			let parsedMessage = JSON.parse(message.body);
-			if (parsedMessage.type == "user-status-alert") showUserStatus(parsedMessage);
-			if (parsedMessage.type == "ready-success") console.log("YOU ARE READY FOR IT");
 
-			// Game Loop - #2
-			if (parsedMessage.type == "ready-room-battle") {
-				$("#whose-turn").prop("disabled", false); 
-				isYourTurn = true;
-			}
-			
-			// Game Loop - #4
-			if (parsedMessage.type == "battle-coordinates") {
-				// handle coordinates and say hit or miss
-				handleBattleCoordinates(parsedMessage.content);
-				drawBoard("opponent-board", opponentBoard);
-				drawBoard("my-board", myBoard);
-			}
-			
-			// Game Loop - #6 - Almost end. End is when 1 user's ships are all gone
-			if (parsedMessage.type == "battle-coordinates-hit") {
-				updateOpponentBoard(parsedMessage.content, 1);
-				console.log(`${parsedMessage.content} was a HIT`);
-				drawBoard("opponent-board", opponentBoard);
-				drawBoard("my-board", myBoard);
-				playShot();	
-				await new Promise(resolve => setTimeout(resolve, 1000)); 
-				playHit();
-			}
-			
-			if (parsedMessage.type == "battle-coordinates-miss") {
-				updateOpponentBoard(parsedMessage.content, 2);
-				console.log(`${parsedMessage.content} was a MISS`);
-				drawBoard("opponent-board", opponentBoard);
-				drawBoard("my-board", myBoard);
-				playShot();	
-				await new Promise(resolve => setTimeout(resolve, 1000)); 
-				playMiss();
-			}
-			
-			// Game Loop - END
-			if (parsedMessage.type == "battle-finish") win();
-			
+			switch(parsedMessage.type) {
+				case "user-status-alert":
+					$("#messages").append("<tr><td>" + parsedMessage.content + ": status" + "</td></tr>"); // show user status
+					break;
+			  	case "ready-room-battle":
+					$("#whose-turn").prop("disabled", false); 
+					isYourTurn = true;
+					break;
+			  	case "battle-coordinates":
+					handleBattleCoordinates(parsedMessage.content);
+					drawBoard("opponent-board", opponentBoard);
+					drawBoard("my-board", myBoard);
+					break;
+			  	case "battle-coordinates-hit":
+					updateOpponentBoard(parsedMessage.content, 1);
+					console.log(`${parsedMessage.content} was a HIT`);
+					drawBoard("opponent-board", opponentBoard);
+					drawBoard("my-board", myBoard);
+					playSound(0);
+					await new Promise(resolve => setTimeout(resolve, 1000)); 
+					playSound(1);
+					break;
+			  	case "battle-coordinates-miss":
+					updateOpponentBoard(parsedMessage.content, 2);
+					console.log(`${parsedMessage.content} was a MISS`);
+					drawBoard("opponent-board", opponentBoard);
+					drawBoard("my-board", myBoard);
+					playSound(0);
+					await new Promise(resolve => setTimeout(resolve, 1000)); 
+					playSound(2);
+					break;
+			  	case "battle-finish":
+					win();
+					break;
+				}
         });
 		
         stompClient.subscribe(`/topic/${roomId}`, (message) => {
 			let parsedMessage = JSON.parse(message.body);
-			
-			if (parsedMessage.type == "chat-message") showChatMessage(parsedMessage);
-			
-			if (["user-status-alert", "user-status-connect", "user-status-disconnect"].includes(parsedMessage.type)) {
-				showUserStatus(parsedMessage);
-			}
 
-			if (parsedMessage.type == "usernames") addOpponentUsername(parsedMessage.content);
-
-			if (parsedMessage.type == "user-status-disconnect") {
-				// opponent left mid game
-				if (roomIsReady) win();
-				removeOpponentUsername();
-			}
-			
-			// Game Loop - #0 - Room is ready
-			if (parsedMessage.type == "ready-room-success") {
-				$('#game-board-prep-board').remove();
-				drawBoard("opponent-board", opponentBoard);
-				drawBoard("my-board", myBoard);
-				roomIsReady = true;
-			}
+			switch(parsedMessage.type) {
+				case "chat-message":
+					$("#messages").append("<tr><td>" + parsedMessage.content + "</td></tr>"); // show user message
+					break;
+			  	case "user-status-alert":
+			  	case "user-status-connect":
+					$("#messages").append("<tr><td>" + parsedMessage.content + ": status" + "</td></tr>"); // show user status
+					break;
+			  	case "usernames":
+					addOpponentUsername(parsedMessage.content);
+					break;
+			  	case "user-status-disconnect":
+					$("#messages").append("<tr><td>" + parsedMessage.content + ": status" + "</td></tr>"); // show user status
+					if (roomIsReady) win(); // opponent left mid game
+					$("#p-vs-p").text("Waiting for opponent...");
+					break;
+			  	case "ready-room-success":
+					$('#game-board-prep-board').remove();
+					drawBoard("opponent-board", opponentBoard);
+					drawBoard("my-board", myBoard);
+					roomIsReady = true;
+					break;
+				}
         });
     });
 }
@@ -147,7 +120,7 @@ function connect() {
 function win() {
   	isGameFinished = true;
 	alert("You won!");
-  	setTimeout(() => { location.reload(); }, 10000);
+  	setTimeout(() => { location.reload(); }, 5000);
 }
 
 function disconnect() {
@@ -159,24 +132,12 @@ function disconnect() {
 	location.reload();
 }
 
-function sendName() {
-    stompClient.send(`/app/message/${roomId}`, {}, JSON.stringify({'content': $("#message").val()}));
-}
-
-function showChatMessage(message) {
-    $("#messages").append("<tr><td>" + message.content + "</td></tr>");
-}
-
-function showUserStatus(message) {
-    $("#messages").append("<tr><td>" + message.content + ": status" + "</td></tr>");
-}
-
 // from list of usernames find and add the opponent's username and enable ready button
 function addOpponentUsername(usernames) {
 	for (const username of usernames) {
 		console.log("usernames: " + username);
 		if (myUsername != username) {
-			document.getElementById("p-vs-p").innerHTML = `Opponent: ${username}`;	
+			$("#p-vs-p").text(`Opponent: ${username}`);
 			if (shipCount == maxShipCount && !isReady) $("#ready-button").prop("disabled", false); 
 			hasOpponent = true;
 			return;	
@@ -184,18 +145,8 @@ function addOpponentUsername(usernames) {
 	}
 }
 
-function removeOpponentUsername() {
-	document.getElementById("p-vs-p").innerHTML = `Waiting for opponent...`;	
-}
-
-function sendReadySignal() {
-    stompClient.send(`/app/ready/${roomId}`, {}, JSON.stringify({}));
-}
-
 function broadcastCoordinates(coordinates) {
-	const row = Number(coordinates.split(",")[0]);
-	const col = Number(coordinates.split(",")[1].charCodeAt(0) - 64); // change from letter to number
-	
+	const {row, col} = convertCoordinates(coordinates);
 	
 	if(isGameFinished){
 		alert("Game is finished");
@@ -213,165 +164,81 @@ function broadcastCoordinates(coordinates) {
 
 function setUpMyCoordinates(coordinates) {
 	if (shipCount < maxShipCount) {
-		const row = Number(coordinates.split(",")[0]);
-		const col = Number(coordinates.split(",")[1].charCodeAt(0) - 64); // change from letter to number
+		const {row, col} = convertCoordinates(coordinates);
 
 		// check if a ship is not in those coordinates already
 		if (myBoard[row - 1][col - 1] == 0) {
 			myBoard[row - 1][col - 1] = 3;
 		
-			drawPrepBoard();
+			drawBoard("prep-board", myBoard);
 			++shipCount;
 		}	
 	} else {
 		alert("That's enough ships!");
 	}
 
-	if (shipCount == maxShipCount && hasOpponent && !isReady){
-		$("#ready-button").prop("disabled", false);
-	}
-}
-
-function drawPrepBoard() {
-	let name = "prep-board";
-	let array = myBoard;
-	$(`#game-board-${name}`).empty();
-	let gameBoard = document.getElementById(`game-board-${name}`);
-	let h1 = document.createElement("h1");
-	h1.innerHTML = name;
-	gameBoard.appendChild(h1);
-
-	// set up letter coordinates A - J
-	let row = document.createElement("div");
-	row.classList.add("row");
-	row.classList.add("w-75");
-	for (let j = 0; j <= 10; j++) {
-		let col = document.createElement("div");
-		col.classList.add("col-1");
-		col.classList.add("border");
-		col.classList.add("border-primary");
-		col.innerHTML = `${String.fromCharCode(j + 64)}`;
-		if (j == 0) col.innerHTML = "";
-		row.appendChild(col);
-	}	
-	gameBoard.appendChild(row);
-	
-	for (let i = 1; i <= 10; i++) {
-		let row = document.createElement("div");
-		row.classList.add("row");
-		row.classList.add("w-75");
-		for (let j = 0; j <= 10; j++) {
-			let col = document.createElement("div");
-			col.classList.add("col-1");
-			col.classList.add("border");
-			col.classList.add("border-primary");
-			col.innerHTML = `${i}`;
-			if (j != 0) {
-				col.classList.add(`${name}-coords`);
-				if (array[i - 1][j - 1] == 1) {
-					col.innerHTML = "💥";
-				} else if (array[i - 1][j - 1] == 2) {
-					col.innerHTML = '❌';
-				} else if (array[i - 1][j - 1] == 3) {
-					col.innerHTML = '🚢';
-				} else {
-					col.innerHTML = '🌊';
-				}
-				col.setAttribute('id', `${i},${String.fromCharCode(j + 64)}`)
-			}
-			
-			row.appendChild(col);
-		}	
-		gameBoard.appendChild(row);
-	}
-	
-	assignBoardOnClickType(name);
+	if (shipCount == maxShipCount && hasOpponent && !isReady) $("#ready-button").prop("disabled", false);
 }
 
 function drawBoard(name, array) {
-	$(`#game-board-${name}`).empty();
 	let gameBoard = document.getElementById(`game-board-${name}`);
+	gameBoard.innerHTML = "";
+
 	let h1 = document.createElement("h1");
-	h1.innerHTML = name;
+	h1.textContent = name;
+
 	gameBoard.appendChild(h1);
 
 	// set up letter coordinates A - J
 	let row = document.createElement("div");
-	row.classList.add("row");
-	row.classList.add("w-75");
+	row.classList.add("row", "w-75");
 	for (let j = 0; j <= 10; j++) {
-		let col = document.createElement("div");
-		col.classList.add("col-1");
-		col.classList.add("border");
-		col.classList.add("border-primary");
-		col.innerHTML = `${String.fromCharCode(j + 64)}`;
-		if (j == 0) col.innerHTML = "";
-		row.appendChild(col);
+		row.insertAdjacentHTML('beforeend', `<div class="col-1 border border-primary">${String.fromCharCode(j + 64)}</div>`);
 	}	
 	gameBoard.appendChild(row);
 	
 	for (let i = 1; i <= 10; i++) {
 		let row = document.createElement("div");
-		row.classList.add("row");
-		row.classList.add("w-75");
+		row.classList.add("row", "w-75");
 		for (let j = 0; j <= 10; j++) {
 			let col = document.createElement("div");
-			col.classList.add("col-1");
-			col.classList.add("border");
-			col.classList.add("border-primary");
-			col.innerHTML = `${i}`;
+			col.classList.add("col-1", "border", "border-primary");
+			col.textContent = i;
 			if (j != 0) {
 				col.classList.add(`${name}-coords`);
 				if (array[i - 1][j - 1] == 1) {
-					col.innerHTML = "💥";
+					col.textContent = "💥";
 				} else if (array[i - 1][j - 1] == 2) {
-					col.innerHTML = '❌';
+					col.textContent = "❌";
 				} else if (array[i - 1][j - 1] == 3) {
-					col.innerHTML = '🚢';
+					col.textContent = "🚢";
 				} else {
-					col.innerHTML = '🌊';
+					col.textContent = "🌊";
 				}
-				col.setAttribute('id', `${i},${String.fromCharCode(j + 64)}`)
+				col.id = `${i},${String.fromCharCode(j + 64)}`;
 			}
-			
 			row.appendChild(col);
 		}	
 		gameBoard.appendChild(row);
 	}
-	
 	assignBoardOnClickType(name);
-	
 }
 
 // depending on the board name different onClick methods will be assigned
 function assignBoardOnClickType(name) {
-	if (name == "opponent-board") {
-		const collections = document.getElementsByClassName("opponent-board-coords");
-		for (const element of collections) {
-			element.addEventListener("click", () => {
-				broadcastCoordinates(element.getAttribute("id"))
-			});
-		}
-	}
-	
-	if (name == "prep-board") {
-		const collections = document.getElementsByClassName("prep-board-coords");
-		for (const element of collections) {
-			element.addEventListener("click", () => {
-				setUpMyCoordinates(element.getAttribute("id"))
-			});
-		}
+	const collections = document.getElementsByClassName(`${name}-coords`);
+	for (const element of collections) {
+		element.addEventListener("click", () => {
+			if (name == "opponent-board") broadcastCoordinates(element.getAttribute("id"))
+			if (name == "prep-board") setUpMyCoordinates(element.getAttribute("id"))
+		});
 	}
 }
 
 
 // Game Loop - #4
 function handleBattleCoordinates(coordinates) {
-	console.log(`handles coordinates ${coordinates}`);
-	const row = Number(coordinates.split(",")[0]);
-	const col = Number(coordinates.split(",")[1].charCodeAt(0) - 64); // change from letter to number
-	
-	console.log(`row: ${row}, col: ${col}`);
+	const {row, col} = convertCoordinates(coordinates);
 	
 	// broadcast a hit(1)/miss(2)
 	if (myBoard[row - 1][col - 1] == 3) {
@@ -385,7 +252,7 @@ function handleBattleCoordinates(coordinates) {
 			stompClient.send(`/app/finish/${roomId}`, {}, JSON.stringify({'content': coordinates}));
 			isGameFinished = true;
 			alert("You lost");
-		  	setTimeout(() => { location.reload(); }, 10000);
+		  	setTimeout(() => { location.reload(); }, 5000);
 		}
 	} else {
 		stompClient.send(`/app/miss/${roomId}`, {}, JSON.stringify({'content': coordinates}));
@@ -395,35 +262,34 @@ function handleBattleCoordinates(coordinates) {
 	}
 }
 
-function updateOpponentBoard(coordinates, hitMiss) {
-	// change your opponent board
+function updateOpponentBoard(coordinates, hitOrMiss) {
+	const {row, col} = convertCoordinates(coordinates);
+	opponentBoard[row - 1][col - 1] = hitOrMiss;
+}
+
+function convertCoordinates(coordinates) {
 	const row = Number(coordinates.split(",")[0]);
 	const col = Number(coordinates.split(",")[1].charCodeAt(0) - 64); // change from letter to number
-	
-	opponentBoard[row - 1][col - 1] = hitMiss;
+	return {row, col};
 }
 
-function playShot () {
-	if (sound) new Audio('/audio/nyoom.mp3').play();
-}
-
-function playHit() {
-	if (sound) new Audio('/audio/hit.mp3').play();
-}
-
-function playMiss() {
-	if (sound) new Audio('/audio/miss.mp3').play();
+function playSound(type) {
+	if (sound) {
+		if (type == 0) 	new Audio('/audio/nyoom.mp3').play()
+		if (type == 1) 	new Audio('/audio/hit.mp3').play()
+		if (type == 2) 	new Audio('/audio/miss.mp3').play()
+	}
 }
 
 $(() => {
-    $("form").on('submit', (e) => {
-        e.preventDefault();
-    });
+    $("form").on('submit', (e) => { e.preventDefault(); });
     $("#connect").click(() => { connect(); });
     $("#disconnect").click(() => { disconnect(); });
-    $("#send").click(() => { sendName(); });
+    $("#send").click(() => { 
+		stompClient.send(`/app/message/${roomId}`, {}, JSON.stringify({'content': $("#message").val()}));
+	});
     $("#ready-button").click(() => { 
-		sendReadySignal(); 
+		stompClient.send(`/app/ready/${roomId}`, {}, JSON.stringify({})); // send ready signal
 		$("#ready-button").prop("disabled", true); 
 		isReady = true;
 	});
